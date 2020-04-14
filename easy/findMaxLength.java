388beae0815132fb9d548b56cc7a99394bb16fbe
Kunal Dutta <kdutta@berkeley.edu>
2020-04-13 23:36:34 -0700

    added contiguous array leetcode problem

---
+++ easy/findMaxLength.java
@@ -0,0 +1,19 @@
+// Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+        return maxlen;
+    }
+}
+    
