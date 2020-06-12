e96681a647688f99f057cc8fcde82381ed5be27e
Kunal Dutta <kdutta@berkeley.edu>
2020-06-11 18:34:53 -0700

    Leetcode 75. Sort Colors

---
+++ medium/SortColors.java
@@ -0,0 +1,33 @@
+/**
+ * Leetcode 75. Sort Colors
+ * Given an array with n objects colored red, white or blue, 
+ * sort them in-place so that objects of the same color are adjacent, 
+ * with the colors in the order red, white and blue.
+ * Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+**/
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0 = 0, count1 = 0, count2 = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                count0++;
+            } else if (num == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i < count0) {
+                nums[i] = 0;
+            } else if (i < count0 + count1) {
+                nums[i] = 1;
+            } else {
+                nums[i] = 2;
+            }
+        }
+        
+    }
+}
