601cafaa86f38f543fd1058319721515261dbf07
Kunal Dutta <kunaldutta1749@gmail.com>
2020-11-03 23:21:17 -0800

    Leetcode 252. Meeting Rooms

---
+++ easy/MeetingRooms.java
@@ -0,0 +1,22 @@
+/* Leetcode 252. Meeting Rooms
+ * Given an array of meeting time intervals 
+ * where intervals[i] = [starti, endi], 
+ * determine if a person could attend all meetings.
+ */
+
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+        public int compare(int[] i1, int[] i2) {
+            return i1[0] - i2[0];
+         }
+        });
+
+        for (int i = 0; i < intervals.length - 1; i++) {
+          if (intervals[i][1] > intervals[i + 1][0])
+            return false;
+        }
+        
+        return true;
+    }
+}
