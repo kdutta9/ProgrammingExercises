a9dbf7edf0faca66ebddd7546fda91e6a9093edc
Kunal Dutta <kunaldutta1749@gmail.com>
2020-12-07 23:47:37 -0800

    Leetcode 121. Best Time to Buy and Sell Stock

---
+++ easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,23 @@
+/* Leetcode 121. Best Time to Buy and Sell Stock
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * If you were only permitted to complete at most one transaction 
+ * (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ * Note that you cannot sell a stock before you buy one.
+*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int low = Integer.MAX_VALUE;
+        int high = 0;
+        
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < low) {
+                low = prices[i];
+            } else if (prices[i] - low > high) {
+                high = prices[i] - low;
+            }
+        }
+        
+        return high;
+    }
+}
